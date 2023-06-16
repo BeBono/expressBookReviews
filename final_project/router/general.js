8f2404fa8e11e23277a529669fcbb88e9410ee6f
BeBono <gitbebono@gmail.com>
2023-06-16 14:10:37 -0500

    Test1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,7 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "Yet to be implemented."});
 });
 
 // Get the book list available in the shop
