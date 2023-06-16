e2adc3cf4e5b56e6d253e1910ff881a4b24a5fc0
BeBono <122927047+BeBono@users.noreply.github.com>
2023-06-16 16:31:45 -0500

    Task1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,11 +10,23 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented....."});
 });
 
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const getBooks = new Promise((resolve, reject) => {  
+
+      resolve(
+        res.send (JSON.stringify(books, null, 4))
+      );
+
+  });
+
+  getBooks.then(() => console.log("The Task 10 has been resolved!")); //call back.
+
 });
+
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
